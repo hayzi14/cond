9806f42951c1c923cf72cf24647248d6f0a7b0f0
hayzi14 <143780539+hayzi14@users.noreply.github.com>
2023-09-29 11:02:32 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,11 +1,8 @@
 a = int(input('Введите первое число: '))
 b = int(input('Введите второе число: '))
 
-
-if a % b == 0: 
-       print('Делится')
-       print(a / b)
+if a % b == 0:
+        print(a,'Делится',b)
+     
 else:
-       print('Не делится')
-       print(a / b)
-              
+        print(b,'Не делится',a)
